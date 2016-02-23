87d4830db26ba9211bea69a452bd484dfe4ee1df
Chris Jimenez <cjimenez16@gmail.com>
2016-02-23 01:29:35 -0600

    Improve code coverage

--- VideoBackgroundViewControllerTests/VideoBackgroundControllerSpec.swift
+++ VideoBackgroundViewControllerTests/VideoBackgroundControllerSpec.swift
@@ -21,9 +21,16 @@ class VideoBackgroundControllerSpec: QuickSpec {
         
         it("is correct with initial values"){
             
+            videoBackgroundViewController.videoShouldLoop = true
+            videoBackgroundViewController.alpha = 0.8
+            videoBackgroundViewController.playSound = false
+            videoBackgroundViewController.videoScalingMode = .ResizeAspectFill
+            videoBackgroundViewController.videoURL = NSURL()
+            videoBackgroundViewController.backgroundColor = UIColor.blackColor()
+            
             expect(videoBackgroundViewController.videoPlayer).notTo(beNil())
-            expect(videoBackgroundViewController.soundLevel).to(equal(1.0))
-            expect(videoBackgroundViewController.playSound).to(beTrue())
+            expect(videoBackgroundViewController.soundLevel).to(equal(0.0))
+            expect(videoBackgroundViewController.playSound).to(beFalse())
             expect(videoBackgroundViewController.videoURL).to(beNil())
             expect(videoBackgroundViewController.videoShouldLoop).to(beTrue())
             expect(videoBackgroundViewController.videoScalingMode).to(equal(VideoScalingMode.ResizeAspectFill))
