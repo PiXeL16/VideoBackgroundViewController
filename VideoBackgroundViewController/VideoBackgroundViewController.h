2b77d138c0cac0bf46bae6fea79f6d158f8de82b
Chris Jimenez <cjimenez16@gmail.com>
2016-02-22 22:07:05 -0600

    Initial Commit

---
+++ VideoBackgroundViewController/VideoBackgroundViewController.h
@@ -0,0 +1,19 @@
+//
+//  VideoBackgroundViewController.h
+//  VideoBackgroundViewController
+//
+//  Created by Chris Jimenez on 2/22/16.
+//  Copyright © 2016 Chris Jimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoBackgroundViewController.
+FOUNDATION_EXPORT double VideoBackgroundViewControllerVersionNumber;
+
+//! Project version string for VideoBackgroundViewController.
+FOUNDATION_EXPORT const unsigned char VideoBackgroundViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoBackgroundViewController/PublicHeader.h>
+
+
